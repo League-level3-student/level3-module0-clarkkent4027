4be357415959a42892ccfacc1c53d23dd8bf7e0b
clarkkent4027 <clarkkent4027@gmail.com>
2021-12-12 16:29:32 -0800

    javaisfun

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -14,12 +14,12 @@ public class _01_IntroToArrays {
 		friends[4] = "Mr. Dave";
 
 		// 2. print the third element in the array
-		System.out.println(friends[2]);
+		// System.out.println(friends[2]);
 
 		// 3. set the third element to a different value
 		friends[2] = "GavinB";
 		// 4. print the third element again
-		System.out.println(friends[2]);
+		// System.out.println(friends[2]);
 		// 5. use a for loop to set all the elements in the array to a string
 		// of your choice
 		for (int i = 0; i < friends.length; i++) {
@@ -28,7 +28,7 @@ public class _01_IntroToArrays {
 		// 6. use a for loop to print all the values in the array
 		// BE SURE TO USE THE ARRAY'S length VARIABLE
 		for (int j = 0; j < friends.length; j++) {
-			System.out.println(friends[j]);
+			// System.out.println(friends[j]);
 		}
 		// 7. make an array of 50 integers
 		int[] ints = new int[50];
@@ -41,12 +41,31 @@ public class _01_IntroToArrays {
 		}
 		// 9. without printing the entire array, print only the smallest number
 		// on the array
-		System.out.println();
+		int smallestnum = ints[0];
+		for (int l = 0; l < ints.length; l++) {
+			if (ints[l] < smallestnum) {
+				smallestnum = ints[l];
+			}
+
+		}
+		// System.out.println(smallestnum);
 		// 10 print the entire array to see if step 8 was correct
-
+		for (int c = 0; c < ints.length; c++) {
+			// System.out.println(ints[c]);
+		}
 		// 11. print the largest number in the array.
+		int largestnum = ints[0];
+		for (int s = 0; s < ints.length; s++) {
+			if (ints[s] > largestnum) {
+				largestnum = ints[s];
+			}
 
+		}
+	//	System.out.println(largestnum);
+		for (int d = 0; d < ints.length; d++) {
+		//	System.out.println(ints[d]);
+		}
 		// 12. print only the last element in the array
-
+		System.out.println(ints[49]);
 	}
 }
--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -7,24 +7,52 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
-    // 1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
 
-        // 2. create an array of 5 robots.
+		// 2. create an array of 5 robots.
+		Robot[] racers = new Robot[5];
+		{
+			// 3. use a for loop to initialize the robots.
+			for (int i = 0; i < racers.length; i++) {
+				racers[i] = new Robot();
+			}
+			// 4. make each robot start at the bottom of the screen, side by side, facing up
+			racers[0].moveTo(100, 550);
+			racers[1].moveTo(250, 550);
+			racers[2].moveTo(400, 550);
+			racers[3].moveTo(550, 550);
+			racers[4].moveTo(700, 550);
+			// 5. use another for loop to iterate through the array and make each robot move
+			// a random amount less than 50.
+			Random Randy = new Random();
 
-        // 3. use a for loop to initialize the robots.
+			racers[0].setSpeed(3);
+			racers[1].setSpeed(3);
+			racers[2].setSpeed(3);
+			racers[3].setSpeed(3);
+			racers[4].setSpeed(3);
 
-        // 4. make each robot start at the bottom of the screen, side by side, facing up
-    
-        // 5. use another for loop to iterate through the array and make each robot move
-        // a random amount less than 50.
-    
-        // 6. use a while loop to repeat step 5 until a robot has reached the top of the
-        // screen.
-    
-        // 7. declare that robot the winner and throw it a party!
-    
-        // 8. try different races with different amounts of robots.
-    
-        // 9. make the robots race around a circular track.
+			// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+			// screen.
+			boolean b = true;
+			while(b==true) {
+				
+			
+			for (int i = 0; i < racers.length; i++) {
+				racers[i].move(Randy.nextInt(50));
+				if (racers[i].getY()<100) {
+					  b = false;
+				}
+					
+				
+			}
+			}
+			// 7. declare that robot the winner and throw it a party!
+JOptionPane.showMessageDialog(null, "YAY!!! Bot 3 won!!!");
+			// 8. try different races with different amounts of robots.
 
+			// 9. make the robots race around a circular track.
+		}
+	}
 }
